7848bb53b7cdfac3476f27602b14bb867cac9b4d
Edwin Geul <edwingeul01@gmail.com>
2022-12-07 15:13:46 -0600

    Agregar archivos de proyecto.

---
+++ FactorialTest/Factorial.Test.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+
+namespace FactorialTest
+{
+    public class Factorial
+    {
+
+
+        [Fact]
+        public void CalcularFactorialCero()
+        {
+            //Arrange 
+            factorialservices servicios = new factorialservices();
+
+            //Act 
+            int result = servicios.calcularFactorial(0);
+            
+
+            //Assert
+            Assert.Equal(1,result);
+
+
+        }
+
+
+        [Fact]
+        public void CalcularFactorialFive()
+        {
+            factorialservices services = new factorialservices();
+
+            int result = services.calcularFactorial(5);
+
+            Assert.Equal(120,result);
+
+
+        }
+    }
+}
---
+++ factorial/factorial.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    public class factorialservices
+    {
+
+    public static void Main(string[] args)
+    {
+
+
+
+
+    }
+
+
+
+
+    public int calcularFactorial(int numeroACalcular)
+    {
+
+        if(numeroACalcular == 0)
+        {
+            return 1;
+        }
+
+        return numeroACalcular * calcularFactorial(numeroACalcular - 1) ;
+
+    }
+
+
+
+}
